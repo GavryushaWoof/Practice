43fa98bb6be5e80347e41f26537b7ca7648350de
GavryushaWoof <gavgav.cat@yandex.ru>
2020-05-09 15:39:08 +0300

    09.05.2020

--- NodeSchool/learnyounode/21.03/2.js
+++
@@ -1,8 +0,0 @@
-function Sum(arr) {
-    let stdout = 0
-    for (let i = 2; i < arr.length; i++) {
-        stdout += +arr[i]
-    }
-    return stdout
-}
-console.log(Sum(process.argv))
--- NodeSchool/learnyounode/21.03/3.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-fs.readFileSync('/path/to/file');
-str = buf.toString()
